94794f08f424ecb07a283680b00c926896dbdba4
Robbie Nohra <rnohra@benchsci.com>
2021-11-27 22:22:54 -0500

    Update

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -13,6 +13,7 @@ alias t="tmux"
 alias ta="t a -t"
 alias tls="t ls"
 alias tn="t new -t"
+alias tk="t kill-server"
 
 # fzf
 
--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 brew tap xwmx/taps # for nb CLI
 brew install $(<packages.txt)
+
+# gh extensions
+
+gh extension install mislav/gh-branch
