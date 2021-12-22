d77efb6085364c820aedce64cd5295dcba4572e4
Robbie Nohra <rnohra@benchsci.com>
2021-12-22 01:28:22 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 brew tap xwmx/taps # for nb, pb CLIs
 brew install $(<packages.txt)
 
+# https://github.com/JetBrains/JetBrainsMono
+# https://ohmyz.sh/#install
+# https://github.com/ColemakMods/mod-dh
+
 # gh extensions
 
 gh extension install mislav/gh-branch
