21fea94710b80e10c3bad0da4b4798d0e343e6a0
Robbie Nohra <rnohra@benchsci.com>
2022-01-22 23:42:19 -0500

    chore: update get comments script

--- get_comments.sh
+++ get_comments.sh
@@ -4,4 +4,6 @@ task_id=$2
 comments=$(curl "https://api.todoist.com/rest/v1/comments?task_id=${task_id}" \
   -H "Authorization: Bearer ${token}")
 
-echo $comments | jq -r '.[] .content' 
+echo $comments | jq -r '.[] | .content, select(.attachment.file_url)' 
+
+# echo $comments
