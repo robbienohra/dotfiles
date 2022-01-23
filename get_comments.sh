659bc7a9cafd28b9d1c901042711ca8e982c47e7
Robbie Nohra <rnohra@benchsci.com>
2022-01-23 00:11:32 -0500

    chore: correct jq filter

--- get_comments.sh
+++ get_comments.sh
@@ -4,6 +4,6 @@ task_id=$1
 comments=$(curl "https://api.todoist.com/rest/v1/comments?task_id=${task_id}" \
   -H "Authorization: Bearer ${token}")
 
-echo $comments | jq -r '.[] | .content, select(.attachment.file_url)' 
+echo $comments | jq -r '.[] | .content, .attachment.file_url? | select(. != null)' 
 
 # echo $comments
