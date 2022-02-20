56f64c749b80f3c37f007214aae9899043be844c
Robbie Nohra <rnohra@benchsci.com>
2022-02-20 17:48:10 -0500

    feat: added json language server

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -1,4 +1,4 @@
-local servers = { "tsserver", "clangd", "pyright", "rust_analyzer", "eslint" }
+local servers = { "tsserver", "clangd", "volar", "eslint", "pyright", "rust_analyzer", "jsonls" }
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, lsp in pairs(servers) do
@@ -8,7 +8,7 @@ for _, lsp in pairs(servers) do
     },
     capabilities = capabilities,
     on_attach = function(client)
-      if client.name == "tsserver" then
+      if client.name == "tsserver" or client.name == "jsonls" then
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
       end
