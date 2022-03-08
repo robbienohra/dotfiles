9355dbf0466dda929960a737b999394a574c5356
Robbie Nohra <rnohra@benchsci.com>
2022-03-07 22:21:42 -0500

    line number highlight

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,7 +1,9 @@
 local cmd = vim.cmd
 cmd("colo gruvbox-baby")
+cmd("highlight LineNr guifg=#665C54")
+vim.opt.bg = "dark"
+-- legacy gruvbox theme
 -- vim.g.gruvbox_contrast_dark = 'medium'
 -- vim.g.gruvbox_invert_selection = 0
 -- vim.g.gruvbox_italic = 1
 -- vim.g.gruvbox_sign_column = 'bg0'
-vim.opt.bg = "dark"
