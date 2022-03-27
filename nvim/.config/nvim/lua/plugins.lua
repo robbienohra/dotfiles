3c69e265d654af62216e9e19928fb4721bd3249d
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-03-26 20:02:23 -0400

    fix: tmux split and org plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,20 +4,39 @@ if not status_ok then
 end
 
 return packer.startup(function(use)
+  use({
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/vim-vsnip",
+    "hrsh7th/vim-vsnip-integ",
+  })
+  use({
+    "tpope/vim-fugitive",
+    "tpope/vim-repeat",
+    "tpope/vim-rhubarb", -- required by fugitive to :Gbrowse
+    "tpope/vim-surround",
+    "tpope/vim-unimpaired",
+    "tpope/vim-vinegar",
+  })
+  use({
+    "junegunn/fzf.vim",
+    { "junegunn/fzf", run = ":call fzf#install()" },
+    "junegunn/vim-peekaboo",
+  })
+  use({
+    "neovim/nvim-lspconfig",
+    "williamboman/nvim-lsp-installer",
+    "jose-elias-alvarez/null-ls.nvim",
+    { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+    { "michaelb/sniprun", run = "bash ./install.sh" },
+  })
+  use({ "luisiacc/gruvbox-baby", branch = "main" })
   use("Yggdroot/indentLine")
-  use("ellisonleao/gruvbox.nvim")
   use("hoob3rt/lualine.nvim")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-cmdline")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-vsnip")
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/vim-vsnip")
-  use("hrsh7th/vim-vsnip-integ")
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("junegunn/fzf.vim")
-  use("junegunn/vim-peekaboo")
   use("kyazdani42/nvim-web-devicons")
   use("mhinz/vim-signify")
   use("norcalli/nvim-colorizer.lua")
@@ -26,20 +45,6 @@ return packer.startup(function(use)
   use("plasticboy/vim-markdown")
   use("reedes/vim-pencil")
   use("rktjmp/lush.nvim")
-  use("tpope/vim-fugitive")
-  use("tpope/vim-repeat")
-  use("tpope/vim-rhubarb") -- required by fugitive to :Gbrowse
-  use("tpope/vim-surround")
-  use("tpope/vim-unimpaired")
-  use("tpope/vim-vinegar")
   use("wbthomason/packer.nvim")
   use("windwp/nvim-autopairs")
-  use({ "junegunn/fzf", run = ":call fzf#install()" })
-  use({ "luisiacc/gruvbox-baby", branch = "main" })
-  use({ "michaelb/sniprun", run = "bash ./install.sh" })
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use({
-    "neovim/nvim-lspconfig",
-    "williamboman/nvim-lsp-installer",
-  })
 end)
