cbdef90f713ed9de50f854d64d0748871708fda0
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-04-04 12:12:03 -0400

    feat: autotag plugin

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -1,5 +1,8 @@
 require("nvim-treesitter.configs").setup({
   ensure_installed = "maintained",
+  autotag = {
+    enable = true,
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = { "markdown" },
