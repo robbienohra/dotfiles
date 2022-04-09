4a4c94dc3d71698df054d29222b09cd3de13a7a3
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-04-09 19:53:10 -0400

    feat: split window bindings

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -6,9 +6,10 @@ cmd("highlight VertSplit cterm=NONE")
 cmd(":command! FixWhitespace :%s/s+$//e")
 
 -- https://github.com/junegunn/fzf.vim/issues/346#issuecomment-288483704
-cmd(
-  [[command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)]]
-)
+cmd([[
+command! -bang -nargs=* Rg call fzf#vim#grep("rg :--column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)
+]])
+
 cmd(":command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)")
 
 -- compile and run c++ program
---
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -0,0 +1,9 @@
+require("telescope").setup({
+  defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      prompt_position = "bottom",
+      mirror = true,
+    },
+  },
+})
