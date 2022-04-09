f66a19cc9af8d492328e346f8520bfaad3626d2f
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-04-09 16:49:16 -0400

    feat: add nvim autocmd

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -1,18 +1,25 @@
-local cmd = vim.cmd
+local cmd = vim.api.nvim_command
+local autocmd = vim.api.nvim_create_autocmd
+
 cmd("highlight VertSplit cterm=NONE")
-
--- compile and run c++ program
-cmd("au FileType cpp nnoremap <C-c> :!g++ -o  %:r.out % -std=c++11<Enter>")
-cmd("au FileType cpp nnoremap <C-x> :!./%:r.out")
-
--- override default python indentation
-cmd("au BufNewFile,BufRead *.py set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab autoindent")
-
--- disable auto-comment
-cmd("au FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o")
 
 cmd(":command! FixWhitespace :%s/s+$//e")
 
 -- https://github.com/junegunn/fzf.vim/issues/346#issuecomment-288483704
-cmd([[command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)]])
+cmd(
+  [[command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)]]
+)
 cmd(":command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)")
+
+-- compile and run c++ program
+autocmd("FileType cpp", { command = "nnoremap <C-c> :!g++ -o  %:r.out % -std=c++11<Enter>" })
+autocmd("FileType cpp", { command = "nnoremap <C-x> :!./%:r.out" })
+
+-- override default python indentation
+autocmd(
+  "BufNewFile,BufRead *.py",
+  { command = "set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab autoindent" }
+)
+
+-- disable auto-comment
+autocmd("FileType *", { command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" })
