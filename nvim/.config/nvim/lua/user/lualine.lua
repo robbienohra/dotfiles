4f6bc45b59634ce5c96cb62b1810d252fdf1e8dc
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-04-13 01:56:48 -0400

    add relative file path to status line

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -1,4 +1,14 @@
 require("lualine").setup({
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- Displays file status (readonly status, modified status)
+        path = 1, -- 0: Just the filename
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+      },
+    },
+  },
   options = {
     theme = "gruvbox_dark",
     icons_enabled = true,
