4dd21b92c47cacb57fb45ecfc867da232c69b653
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-04-20 17:29:42 -0400

    add wezterm configs

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+local scheme = wezterm.get_builtin_color_schemes()["Gruvbox Dark"]
+scheme.background = "#282828"
+return {
+  color_schemes = {
+    ["Gruvbox Dark"] = scheme,
+  },
+  color_scheme = "Gruvbox Dark",
+  -- font = wezterm.font("JetBrainsMonoNL Nerd Font Mono", { weight = "Regular" }),
+  font = wezterm.font("JetBrains Mono", { bold = true }),
+  keys = {
+    { key = "w", mods = "CMD", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+  },
+  window_decorations = "RESIZE",
+  font_size = 13.5,
+  window_background_opacity = 1.0,
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+  },
+  window_close_confirmation = "NeverPrompt",
+}
