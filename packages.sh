2afef11dc1837c26bca947e27752406a69e690ae
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-08-14 23:21:38 -0400

    chore: tmux pane cycle
    - change mode of pane cycling

--- packages.sh
+++ packages.sh
@@ -2,5 +2,6 @@
 # https://github.com/ryanoasis/nerd-fonts
 brew tap homebrew/cask-fonts
 brew install --force --cask font-jetbrains-mono
+brew tap out-of-cheese-error/gooseberry && brew install gooseberry
 brew install --force $(<packages)
 $(brew --prefix)/opt/fzf/install
