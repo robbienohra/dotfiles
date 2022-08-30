1577448a086d60a8a6e52395819a972ae0e304dd
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-08-29 23:20:31 -0400

    chore: rm history widget override

--- zshrc/zshrc/fzf.sh
+++ zshrc/zshrc/fzf.sh
@@ -115,21 +115,3 @@ function join-lines() {
 } b h
 
 bindkey '^E' fzf-file-widget
-
-# CTRL-R - Paste the selected command from history into the command line
-# https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh#L97
-function fzf-history-widget() {
-  local selected num
-  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
-  selected=( $(fc -rln 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
-  local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
-    fi
-  fi
-  zle reset-prompt
-  return $ret
-}
