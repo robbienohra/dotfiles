48f21479058e78b99977b2194ee1acf642e267d1
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-08-31 22:02:54 -0400

    chore: more sh formatting

--- zshrc/zshrc/utils.sh
+++ zshrc/zshrc/utils.sh
@@ -1,9 +1,9 @@
 function srr() {
-  sudo -A rm -r "$@";
+  sudo -A rm -r "$@"
 }
 
-function ch () {
-  open http://google.com;
+function ch() {
+  open http://google.com
 }
 
 function npm_login() {
@@ -11,15 +11,15 @@ function npm_login() {
 }
 
 USER="postgres"
-function u () {
-  DB=$1;
-  usql "postgres://${USER}@localhost:5432/${DB}?sslmode=disable";
+function u() {
+  DB=$1
+  usql "postgres://${USER}@localhost:5432/${DB}?sslmode=disable"
 }
 
-function u-cols () {
-  DB=$1;
-  TABLE=$2;
-  read -r -d '' VAR << EOM
+function u-cols() {
+  DB=$1
+  TABLE=$2
+  read -r -d '' VAR <<EOM
   SELECT column_name
   FROM information_schema.columns 
   WHERE table_schema = 'public' 
@@ -29,22 +29,23 @@ EOM
   usql "postgres://${USER}@localhost:5432/${DB}?sslmode=disable" -c $VAR
 }
 
-function lsp () {
-  tail -f ~/.cache/nvim/lsp.log;
+function lsp() {
+  tail -f ~/.cache/nvim/lsp.log
 }
 
-function dev () {
-  npm run dev;
+function dev() {
+  npm run dev
 }
 
-function re () {
+function re() {
   GOOGLE_PROJECT_ID=robbie-329220 \
-  GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/robbie-329220-d0e67915e0de.json \
-  RESTIC_PASSWORD=$(pass show restic) \
-  restic -r gs:robbie-backups:/restic "$@";
+    GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/robbie-329220-d0e67915e0de.json \
+    RESTIC_PASSWORD=$(pass show restic) \
+    restic -r gs:robbie-backups:/restic "$@"
 }
 
-function rand_str () {
-# https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
- LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 10 ; echo ''
+function rand_str() {
+  # https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
+  LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 10
+  echo ''
 }
