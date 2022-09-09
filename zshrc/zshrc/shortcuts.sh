1a65ced1ad7d99f49f7f00cdd0b3c4afd8906b9a
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-09-08 21:10:18 -0400

    chore: remove shortcuts

--- zshrc/zshrc/shortcuts.sh
+++ zshrc/zshrc/shortcuts.sh
@@ -1,23 +1,3 @@
-function co () {
-  cd $HOME/code;
-}
-
-function my () {
-  cd $HOME/code/my.clearbanc.com;
-}
-
-function comp () {
-  cd $HOME/clear-components;
-}
-
-function dl () {
-  cd $HOME/dotfiles
-}
-
-function alg () {
-  cd $HOME/algorithms
-}
-
 function plugins () {
   cd $HOME/.local/share/nvim/site/pack/packer/start
 }
