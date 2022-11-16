ef3be27aeac57250d86a968513789a8da69f0e56
Robbie Nohra <robbie.nohra@clearbanc.com>
2022-11-15 19:41:12 -0500

    add helm alias

--- zshrc/zshrc/aliases.sh
+++ zshrc/zshrc/aliases.sh
@@ -4,8 +4,13 @@ alias g='git'
 alias b="bash"
 alias v="nvim"
 alias n="nvim ."
+
+# exa
 alias l="exa -l --icons"
 alias la="exa -la"
 alias lt="exa --tree"
+
+# infra
 alias k="kubectl"
+alias h="helm"
 alias tf="terraform"
