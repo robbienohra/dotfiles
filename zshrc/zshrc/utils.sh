4a691e757c72b7d197fd1837998202d3fa049166
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-01-11 13:11:24 -0500

    jwt-decode util

--- zshrc/zshrc/utils.sh
+++ zshrc/zshrc/utils.sh
@@ -54,3 +54,7 @@ function rand_str() {
 function snip() {
   pbpaste | gh gist create -f "$1"
 }
+
+function jwt-decode() {
+  sed 's/\./\n/g' <<< $(cut -d. -f1,2 <<< "$1") | base64 --decode | jq
+}
