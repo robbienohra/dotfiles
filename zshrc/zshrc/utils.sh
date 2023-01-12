bfce4b653848cc421edd80e70b129595ee015058
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-01-11 22:59:06 -0500

    more token utils

--- zshrc/zshrc/utils.sh
+++ zshrc/zshrc/utils.sh
@@ -55,11 +55,31 @@ function snip() {
   pbpaste | gh gist create -f "$1"
 }
 
-function jwt-decode() {
-  sed 's/\./\n/g' <<<$(cut -d. -f1,2 <<<"$1") | base64 --decode | jq
-}
-
 function gen-pairs() {
   openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
   openssl rsa -in private.pem -pubout -out public.pem
+}
+
+function gen-token() {
+  PEM=$(cat $1)
+  GITHUB_APP_ID=4 # Whatever your github app id is
+
+  NOW=$(date +%s)
+  IAT="${NOW}"
+  # expire 9 minutes in the future. 10 minutes is the max for github
+  EXP=$((${NOW} + 540))
+  HEADER_RAW='{"alg":"RS256"}'
+  HEADER=$(echo -n "${HEADER_RAW}" | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+  PAYLOAD_RAW='{"iat":'"${IAT}"',"exp":'"${EXP}"',"iss":'"${GITHUB_APP_ID}"'}'
+  PAYLOAD=$(echo -n "${PAYLOAD_RAW}" | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+  HEADER_PAYLOAD="${HEADER}"."${PAYLOAD}"
+  SIGNATURE=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${HEADER_PAYLOAD}") | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+  JWT="${HEADER_PAYLOAD}"."${SIGNATURE}"
+}
+
+function jwtd() {
+  if [[ -x $(command -v jq) ]]; then
+    jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<<"${1}"
+    echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
+  fi
 }
