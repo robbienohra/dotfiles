b3efc73599b80eeb8ed8ba16bf4b061aa4a089fd
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-01-15 15:15:00 -0500

    fix: gen-token utility

--- zshrc/zshrc/utils.sh
+++ zshrc/zshrc/utils.sh
@@ -62,7 +62,7 @@ function gen-pairs() {
 
 function gen-token() {
   PEM=$(cat $1)
-  ISSUER="foo" # Whatever your github app id is
+  ISSUER="foo"
 
   NOW=$(date +%s)
   IAT="${NOW}"
@@ -70,11 +70,11 @@ function gen-token() {
   EXP=$((${NOW} + 540))
   HEADER_RAW='{"alg":"RS256"}'
   HEADER=$(echo -n "${HEADER_RAW}" | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
-  PAYLOAD_RAW='{"iat":'"${IAT}"',"exp":'"${EXP}"',"iss":'"${ISSUER}"'}'
+  PAYLOAD_RAW='{"iat":'"${IAT}"',"exp":'"${EXP}"',"iss":'\""${ISSUER}"\"'}'
   PAYLOAD=$(echo -n "${PAYLOAD_RAW}" | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
   HEADER_PAYLOAD="${HEADER}"."${PAYLOAD}"
   SIGNATURE=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${HEADER_PAYLOAD}") | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
-  JWT="${HEADER_PAYLOAD}"."${SIGNATURE}"
+  export TOKEN="${HEADER_PAYLOAD}"."${SIGNATURE}"
 }
 
 function jwtd() {
