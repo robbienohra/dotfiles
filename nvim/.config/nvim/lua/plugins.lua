50102c5f026eafb94347446ad514cd3c0398fe14
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-01-19 23:49:52 -0500

    add peekaboo register plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -65,6 +65,7 @@ return require("packer").startup(function(use)
     "williamboman/mason-lspconfig.nvim",
     "windwp/nvim-autopairs",
     "windwp/nvim-ts-autotag",
+    'junegunn/vim-peekaboo',
     { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
   }
   use {
