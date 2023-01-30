0ee67eae25466c7980d05ee080b5f9b81f7efe54
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-01-29 19:40:50 -0500

    remap next history

--- zshrc/zshrc/fzf.sh
+++ zshrc/zshrc/fzf.sh
@@ -10,7 +10,7 @@ export FZF_DEFAULT_OPTS="
 --history=$HOME/.fzf_history \
 --bind ctrl-d:page-down,ctrl-u:page-up \
 --bind alt-p:previous-history \
---bind alt-n:next-history \
+--bind alt-f:next-history \
 --bind ctrl-/:toggle-preview \
 --color='spinner:#fb4934,\
 hl:#928374,\
