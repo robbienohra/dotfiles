63ef1d43ef5c4bcfd806cbcb6f0d9fd1b60e36e8
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-03-09 02:16:16 -0500

    rm diff script

--- diff.sh
+++
@@ -1,7 +0,0 @@
-FILE=$1
-REPO=$(gh repo view --json url | jq '.url' | tr -d '"')
-PR=$(gh pr view --json number | jq '.number')
-SHA=$(echo -n $FILE | sha256sum | head -c 64)
-URL="$REPO/pull/$PR/files/#diff-$SHA"
-
-open $URL
