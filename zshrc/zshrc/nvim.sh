82306e23f9e37dbd0de7915406e871139c417e81
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-03-09 18:49:37 -0500

    chore: rm nvim-dap alias

--- zshrc/zshrc/nvim.sh
+++ zshrc/zshrc/nvim.sh
@@ -2,4 +2,4 @@
 # nvim-dap
 #######################
 
-alias codelldb="while sleep 1; do $(find $HOME/.vscode/extensions -name codelldb) --port 13000 --liblldb $(find $HOME/.vscode/extensions/ -name liblldb.dylib); done"
+# alias codelldb="while sleep 10; do $(find $HOME/.vscode/extensions -name codelldb) --port 13000 --liblldb $(find $HOME/.vscode/extensions/ -name liblldb.dylib); done"
