354a8eb067235aecfb4a6668c36915e732dafee4
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-04-12 13:50:44 -0400

    mod gb util

--- zshrc/zshrc/fzf.sh
+++ zshrc/zshrc/fzf.sh
@@ -55,19 +55,11 @@ export FZF_DEFAULT_OPTS="
 export FZF_DEFAULT_COMMAND="rg --files --follow" # picked up by vim
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-function fzf-down() {
-  fzf --height 50% --min-height 20 --border --bind ctrl-/:toggle-preview "$@"
-}
-# https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
 function is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
 function gb() {
   is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  fzf-down --ansi --multi --tac --preview-window down:60% \
-    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#^remotes/origin/##'
+  g sw $(git branch -a --color=always | grep -v '/HEAD\s' | sort | fzf)
 }
