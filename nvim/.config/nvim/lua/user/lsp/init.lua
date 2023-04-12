2d9dbda34cf5cc116859c593d60015c95cdbfeff
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-04-11 20:54:48 -0400

    add mason tool installer

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -1,6 +1,25 @@
 local util = require "lspconfig.util"
 local lspconfig = require "lspconfig"
 
+require("mason-tool-installer").setup {
+
+  ensure_installed = {
+    "clang_format",
+    "deno_fmt",
+    "prettier",
+    "shfmt",
+    "sql_formatter",
+    "stylua",
+    "terraform_fmt",
+    "yamlfmt",
+  },
+
+  auto_update = false,
+  run_on_start = true,
+  -- start_delay = 3000, -- 3 second delay
+  -- debounce_hours = 5, -- at least 5 hours between attempts to install/update
+}
+
 require("mason").setup {}
 
 require("mason-lspconfig").setup {
