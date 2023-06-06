1386bab7608d5a21d9f33c491a12bed022293027
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-06-06 09:41:03 -0400

    feat: add a wipereg utility

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -3,6 +3,9 @@ local autocmd = vim.api.nvim_create_autocmd
 
 cmd ":command! FixWhitespace :%s/s+$//e"
 
+-- https://stackoverflow.com/questions/19430200/how-to-clear-vim-registers-effectively
+cmd ":command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor"
+
 -- https://vim.fandom.com/wiki/Invoke_a_function_with_a_count_prefix
 vim.api.nvim_exec(
   [[
