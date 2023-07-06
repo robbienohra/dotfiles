3d76f1ae24e736c3a13349a06c41a3c71e89aef8
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-07-05 20:38:40 -0400

    rm work directory

--- work/forward.sh
+++
@@ -1,6 +0,0 @@
-cd ~/code || exit
-gcloud config set project clearbanc-api-staging && kubectl config use-context gke_clearbanc-api-staging_us-central1_staging-core
-gcloud container clusters get-credentials staging-core --region=us-central1
-kubectl port-forward -n default svc/api-service 3333:80 & 
-# kubectl port-forward -n default svc/insights-service 3335:80 &
-# docker compose up my.clearbanc.com --force-recreate
