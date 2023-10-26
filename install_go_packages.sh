ce8ef0e037de3d489c5cdc7fdfbf984b63c39072
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-10-26 10:09:50 -0400

    chore: add k9s

--- install_go_packages.sh
+++ install_go_packages.sh
@@ -3,3 +3,4 @@ go install github.com/mattn/efm-langserver@latest
 go install github.com/mikefarah/yq/v4@latest
 go install github.com/xo/usql@latest
 go install github.com/gohugoio/hugo@latest
+go install github.com/derailed/k9s@latest
