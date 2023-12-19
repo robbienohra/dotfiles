8f73483073cb77e30cfac77313049cd07adcd89e
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-12-18 23:53:11 -0500

    setup harpoon aliases

--- .config/nvim/lua/robbie/plugins/harpoon.lua
+++ .config/nvim/lua/robbie/plugins/harpoon.lua
@@ -4,10 +4,11 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local harpoon = require 'harpoon'
-		harpoon:setup {}
-		vim.keymap.set('n', '<leader>a', function()
+		harpoon:setup()
+		vim.keymap.set('n', '<leader>t', function()
 			harpoon:list():append()
 		end)
+
 		vim.keymap.set('n', '<C-e>', function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
