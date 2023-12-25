fb09d0c1185910800feabbb38f539b7fc81e06c2
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-12-25 01:07:22 -0500

    revert laststatus option

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,9 +1,9 @@
-vim.g.mapleader = ","
+vim.g.mapleader = ','
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.netrw_banner = 0
-local udir = os.getenv "HOME" .. "/undodir"
+local udir = os.getenv 'HOME' .. '/undodir'
 local o = vim.opt
 o.lazyredraw = true
 o.backup = false
@@ -19,14 +19,14 @@ o.termguicolors = true
 o.undofile = true
 o.cmdheight = 0
 o.conceallevel = 2
-o.completeopt = { "menuone", "noinsert", "noselect" }
+o.completeopt = { 'menuone', 'noinsert', 'noselect' }
 -- https://neovim.io/doc/user/change.html#fo-table
 -- o.formatoptions:append "l"
 -- can also append multiple values using a table
 -- o.formatoptions:append { "l", "t" }
 -- can also remove options
 -- o.formatoptions:remove "t"
-o.laststatus = 0
+-- o.laststatus = 0
 o.regexpengine = 0
 o.synmaxcol = 3000
 o.shiftwidth = 2
@@ -34,13 +34,13 @@ o.timeoutlen = 300
 o.ttimeoutlen = 0
 o.undodir = udir
 o.updatetime = 100
-o.clipboard = { "unnamed", "unnamedplus" }
+o.clipboard = { 'unnamed', 'unnamedplus' }
 -- Searching
 o.ignorecase = true
 o.smartcase = true
 -- Folding
 -- https://neovim.io/doc/user/fold.html#[z
 o.foldlevel = 20
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldmethod = 'expr'
+o.foldexpr = 'nvim_treesitter#foldexpr()'
 o.incsearch = true
