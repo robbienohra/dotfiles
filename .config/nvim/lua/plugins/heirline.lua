d34d9395a7fa65daeda18c5a84afc984f1dec7f8
Robbie Nohra <robbie.nohra@clearbanc.com>
2023-12-26 12:59:32 -0500

    change heirline width percent to 0.5

--- .config/nvim/lua/plugins/heirline.lua
+++ .config/nvim/lua/plugins/heirline.lua
@@ -235,7 +235,7 @@ return {
 				local icon = (vim.fn.haslocaldir(0) == 1 and 'l' or 'g') .. ' ' .. ' '
 				local cwd = vim.fn.getcwd(0)
 				cwd = vim.fn.fnamemodify(cwd, ':~')
-				if not conditions.width_percent_below(#cwd, 0.25) then
+				if not conditions.width_percent_below(#cwd, 0.50) then
 					cwd = vim.fn.pathshorten(cwd)
 				end
 				local trail = cwd:sub(-1) == '/' and '' or '/'
