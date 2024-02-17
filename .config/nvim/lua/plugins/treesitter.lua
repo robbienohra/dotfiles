1cbc058459f2ed8e3794256df698b898885cbed9
Robbie Nohra <robbie@robbienohra.com>
2024-02-17 18:01:41 -0500

    more treesitter keymaps

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -80,6 +80,35 @@ return {
 						},
 						include_surrounding_whitespace = true,
 					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							[']m'] = '@function.outer',
+							[']]'] = { query = '@class.outer', desc = 'Next class start' },
+							[']o'] = '@loop.*',
+							[']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+							[']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+						},
+						goto_next_end = {
+							[']M'] = '@function.outer',
+							[']['] = '@class.outer',
+						},
+						goto_previous_start = {
+							['[m'] = '@function.outer',
+							['[['] = '@class.outer',
+						},
+						goto_previous_end = {
+							['[M'] = '@function.outer',
+							['[]'] = '@class.outer',
+						},
+						goto_next = {
+							[']d'] = '@conditional.outer',
+						},
+						goto_previous = {
+							['[d'] = '@conditional.outer',
+						},
+					},
 				},
 			}
 
