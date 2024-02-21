118e33a7553ee0fcfd89baef15bf24ee6f68a40c
Robbie Nohra <robbie@robbienohra.com>
2024-02-20 20:40:21 -0500

    more keymap cleanup

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -21,16 +21,13 @@ return {
 			},
 			mapping = {
 				['<Left>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-				['<Right>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-				['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-				['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-				['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+				['<Right>'] =cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+				['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				['<C-e>'] = cmp.mapping {
 					i = cmp.mapping.abort(),
 					c = cmp.mapping.close(),
 				},
-				['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
 				['<Tab>'] = cmp.mapping(function(fallback)
 					if vim.fn['vsnip#available'](1) == 1 then
 						feedkey('<Plug>(vsnip-expand-or-jump)', '')
