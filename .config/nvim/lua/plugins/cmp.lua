d203402c7dce5588c5ed16efc570365f21cbc040
Robbie Nohra <robbie@robbienohra.com>
2024-04-26 16:31:02 -0400

    remove copilot plugin

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -45,7 +45,6 @@ return {
 				end, { 'i', 's' }),
 			},
 			sources = cmp.config.sources({
-				{ name = 'copilot' },
 				{ name = 'nvim_lsp' },
 				{ name = 'vsnip' }, -- For vsnip users.
 			}, {
