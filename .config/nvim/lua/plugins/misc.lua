deb34e81670db0a06a8fe0b48028794c70497f41
Robbie Nohra <robbie@robbienohra.com>
2024-05-23 02:47:39 -0400

    Revert "remove comment.nvim"
    This reverts commit a1e7f8151a0aee7af488abd3809d30c30f9dd735.

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -61,6 +61,12 @@ return {
 			end,
 		},
 		{
+			'numToStr/Comment.nvim',
+			config = function()
+				require('Comment').setup()
+			end,
+		},
+		{
 			'catppuccin/nvim',
 			name = 'catppuccin',
 			priority = 1000,
