bb9807f9fd267d864d07130a6a36e32209b49214
Robbie Nohra <robbie@robbienohra.com>
2024-07-15 17:28:20 -0400

    remap buffer deletion keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -52,7 +52,7 @@ map('n', '<Leader>q', vim.cmd.quit)
 map('n', '<leader>qa', function()
 	vim.cmd 'qa'
 end)
-map('n', '<Leader>bd', vim.cmd.bdelete)
+map('n', 'q', vim.cmd.bdelete)
 map('n', '<Leader>Q', function()
 	local bufnr = vim.api.nvim_get_current_buf()
 	vim.api.nvim_buf_delete(bufnr, { force = true })
