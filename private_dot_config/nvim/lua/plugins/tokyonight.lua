25af3e5b9ed18ff5c1e919ae7942f59e86c80560
Robbie Nohra <robbie@robbienohra.com>
2024-08-04 00:52:53 -0400

    Update .config/nvim/lua/plugins/tokyonight.lua

--- private_dot_config/nvim/lua/plugins/tokyonight.lua
+++ private_dot_config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,12 @@ return {
 	priority = 1000,
 	opts = {},
 	config = function()
+		require('tokyonight').setup {
+			style = 'moon',
+			on_colors = function(colors)
+				colors.bg_statusline = colors.bg
+			end,
+		}
 		vim.cmd [[colorscheme tokyonight]]
 	end,
 }
