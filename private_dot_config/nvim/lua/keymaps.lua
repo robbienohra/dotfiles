916c4cc6e69b105d586eab766fbf2feabe529370
Robbie Nohra <robbie@robbienohra.com>
2024-08-06 08:00:27 -0400

    Update .config/nvim/lua/keymaps.lua

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -46,7 +46,16 @@ map('n', '<leader>sv', vim.cmd.vsplit)
 
 map('n', '<Leader><cr>', vim.cmd.nohlsearch)
 map('n', '<Leader>s', vim.cmd.update)
-map('n', '<Leader>x', '<cmd>%bd<cr>')
+-- map('n', '<Leader>x', '<cmd>%bd<cr>')
+map('n', '<leader>x', function()
+	local bufs = vim.api.nvim_list_bufs()
+	local current_buf = vim.api.nvim_get_current_buf()
+	for _, i in ipairs(bufs) do
+		if i ~= current_buf then
+			vim.api.nvim_buf_delete(i, {})
+		end
+	end
+end)
 
 -- quit
 map('n', '<Leader>q', vim.cmd.quit)
