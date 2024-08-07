fab19f1b78c8ea7d51f75f1fc6fe8030d862fe4e
Robbie Nohra <robbie@robbienohra.com>
2024-08-06 23:20:19 -0400

    Update .config/nvim/lua/keymaps.lua

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -63,8 +63,8 @@ map('n', '<leader>qa', function()
 	vim.cmd 'qa'
 end)
 map('n', 'q', vim.cmd.bdelete)
-map('n', '<C-m>', vim.cmd.mksession)
-map('n', '<C-s>', function()
+map('n', '<C-s>', vim.cmd.mksession)
+map('n', '<C-s>s', function()
 	vim.cmd [[source Session.vim]]
 end)
 map('n', '<Leader>Q', function()
