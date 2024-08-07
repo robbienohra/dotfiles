c347c49b101fddc7d4ccca1b92ec2bc367da76c3
Robbie Nohra <robbie@robbienohra.com>
2024-08-06 21:35:28 -0400

    add mkd preview

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -3,6 +3,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.netrw_banner = 0
+vim.g.mkdp_auto_start = 1
 local udir = os.getenv 'HOME' .. '/undodir'
 local o = vim.opt
 o.lazyredraw = true
