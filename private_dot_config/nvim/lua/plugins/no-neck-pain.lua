210f9919078b84939923d14c37ede794a45dd214
Robbie Nohra <robbie@robbienohra.com>
2024-08-07 21:37:46 -0400

    Update .config/nvim/lua/plugins/no-neck-pain.lua

--- private_dot_config/nvim/lua/plugins/no-neck-pain.lua
+++ private_dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,1 +1,7 @@
-return { 'shortcuts/no-neck-pain.nvim', tag = '*' }
+return {
+	'shortcuts/no-neck-pain.nvim',
+	version = '*',
+	config = function()
+		require('lazy').setup { { 'shortcuts/no-neck-pain.nvim', version = '*' } }
+	end,
+}
