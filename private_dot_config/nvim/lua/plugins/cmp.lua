7231a29dfc08d3b66d270ab3ddb81aaa9678ba07
Robbie Nohra <robbie@robbienohra.com>
2024-12-10 15:39:31 -0500

    Update .config/nvim/lua/plugins/cmp.lua

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -4,11 +4,6 @@ return {
 	config = function()
 		local cmp = require 'cmp'
 
-		local has_words_before = function()
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-		end
-
 		local feedkey = function(key, mode)
 			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 		end
@@ -31,8 +26,6 @@ return {
 				['<Tab>'] = cmp.mapping(function(fallback)
 					if vim.fn['vsnip#available'](1) == 1 then
 						feedkey('<Plug>(vsnip-expand-or-jump)', '')
-					elseif has_words_before() then
-						cmp.complete()
 					else
 						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
