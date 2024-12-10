826052821bdee7d04f90c0f4a5d73a72244cc6d7
Robbie Nohra <robbie@robbienohra.com>
2024-12-10 13:57:51 -0500

    Update .config/nvim/lua/plugins/cmp.lua Remove .config/nvim/lua/plugins/snip.lua

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -9,14 +9,10 @@ return {
 			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
 		end
 
-		local feedkey = function(key, mode)
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-		end
-
 		cmp.setup {
 			snippet = {
 				expand = function(args)
-					vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
+					vim.snippet.expand(args.body)
 				end,
 			},
 			mapping = {
@@ -29,24 +25,15 @@ return {
 				},
 				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
 				['<Tab>'] = cmp.mapping(function(fallback)
-					if vim.fn['vsnip#available'](1) == 1 then
-						feedkey('<Plug>(vsnip-expand-or-jump)', '')
-					elseif has_words_before() then
+					if has_words_before() then
 						cmp.complete()
 					else
 						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
 				end, { 'i', 's' }),
-
-				['<S-Tab>'] = cmp.mapping(function()
-					if vim.fn['vsnip#jumpable'](-1) == 1 then
-						feedkey('<Plug>(vsnip-jump-prev)', '')
-					end
-				end, { 'i', 's' }),
 			},
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' }, -- For vsnip users.
 			}, {
 				{ name = 'buffer' },
 			}),
@@ -80,8 +67,5 @@ return {
 		'hrsh7th/cmp-cmdline',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-vsnip',
-		'hrsh7th/vim-vsnip',
-		'hrsh7th/vim-vsnip-integ',
 	},
 }
