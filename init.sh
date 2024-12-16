efcfe7c09e42b735777fa530fc6599a67839b9b3
Robbie Nohra <robbie@robbienohra.com>
2024-12-15 21:49:51 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 curl https://sh.rustup.rs -sSf | bash -s -- -y --no-modify-path
 source "$HOME/.cargo/env"
 cargo install nu --locked
-cargo install zellij
-cargo install just
+cargo install zellij --locked
+cargo install just --locked
