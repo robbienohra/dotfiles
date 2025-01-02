7b59d4c1b5915e0c622205065ed4754a8dfcead9
Robbie Nohra <robbie@robbienohra.com>
2025-01-02 18:39:36 -0500

    Add .config/fish/completions/fisher.fish Add .config/fish/completions/fzf_configure_bindings.fish Add .config/fish/completions/nvm.fish Add .config/fish/conf.d/fzf.fish Add .config/fish/conf.d/git_abbr.fish Add .config/fish/conf.d/nvm.fish Add .config/fish/conf.d/rustup.fish Add .config/fish/config.fish Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/__bass.py Add .config/fish/functions/_fzf_configure_bindings_help.fish Add .config/fish/functions/_fzf_extract_var_info.fish Add .config/fish/functions/_fzf_preview_changed_file.fish Add .config/fish/functions/_fzf_preview_file.fish Add .config/fish/functions/_fzf_report_diff_type.fish Add .config/fish/functions/_fzf_report_file_type.fish Add .config/fish/functions/_fzf_search_directory.fish Add .config/fish/functions/_fzf_search_git_log.fish Add .config/fish/functions/_fzf_search_git_status.fish Add .config/fish/functions/_fzf_search_history.fish Add .config/fish/functions/_fzf_search_processes.fish Add .config/fish/functions/_fzf_search_variables.fish Add .config/fish/functions/_fzf_wrapper.fish Add .config/fish/functions/_nvm_index_update.fish Add .config/fish/functions/_nvm_list.fish Add .config/fish/functions/_nvm_version_activate.fish Add .config/fish/functions/_nvm_version_deactivate.fish Add .config/fish/functions/bass.fish Add .config/fish/functions/fish_prompt.fish Add .config/fish/functions/fisher.fish Add .config/fish/functions/fzf_configure_bindings.fish Add .config/fish/functions/nvim.fish Add .config/fish/functions/nvm.fish Add .config/fish/functions/br.fish Add .config/fish/themes/.keep

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr -a ch 'chezmoi'
+abbr -a n 'nvim .'
---
+++ private_dot_config/private_fish/functions/symlink_br.fish
@@ -0,0 +1,1 @@
+/Users/robbienohra/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
