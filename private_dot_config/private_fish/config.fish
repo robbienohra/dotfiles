bf6469e16cb0aca98b9eac24cb10afb5976b8482
Robbie Nohra <robbie@robbienohra.com>
2025-01-02 22:06:22 -0500

    Add .config/fish/config.fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fzf --fish | source
+
+abbr -a ch 'chezmoi'
+abbr -a n 'nvim'
+
+set -x FZF_DEFAULT_OPTS \
+    "--history=$HOME/.fzf_history" \
+    "--bind=ctrl-d:page-down,ctrl-u:page-up" \
+    "--bind=ctrl-p:previous-history" \
+    "--bind=ctrl-f:next-history" \
+    "--bind=F2:toggle-preview" \
+    "--info=hidden" \
+    "--color=bg:#1a1b26" \
+    "--color=bg+:#1a1b26"
